2fee3a7654bc49fe890e6c3654cd31689fccf744
hyungjunn <hotfirebulldog@gmail.com>
2025-01-14 04:52:43 +0900

    refactor: 변수 인라인하기
    * 테스트 코드의 효용성을 느끼게 해준 곳.
    * 마치 역정규화하듯이 세군데에 다시 palyFor(perf)를 인라인해준다.

--- src/javascript/ch01/statement.js
+++ src/javascript/ch01/statement.js
@@ -35,15 +35,14 @@ function statement(invoice, plays) {
     }
 
     for (let perf of invoice.performances) {
-        const play = playFor(perf);
-        let thisAmount = amountFor(perf, play);
+        let thisAmount = amountFor(perf, playFor(perf));
 
         volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === play.type) {
+        if ("comedy" === playFor(perf).type) {
             volumeCredits += Math.floor(perf.audience / 5);
         }
 
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
 
