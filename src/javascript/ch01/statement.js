daf617e0ecf60cee0ab9bff9cbf9064b64ea7d20
hyungjunn <hotfirebulldog@gmail.com>
2025-01-16 22:17:48 +0900

    refactor: move volumeCredits variable declaration right before the loop

--- src/javascript/ch01/statement.js
+++ src/javascript/ch01/statement.js
@@ -1,6 +1,5 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     const usd = new Intl.NumberFormat("en-US",
         {
@@ -48,6 +47,7 @@ function statement(invoice, plays) {
         totalAmount += amountFor(perf);
     }
 
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
