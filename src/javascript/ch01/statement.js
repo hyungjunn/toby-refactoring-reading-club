4f1a4331bf5ba34399096d9a733aedfbe9347bf9
hyungjunn <hotfirebulldog@gmail.com>
2025-01-17 13:18:29 +0900

    refactor: create createStatementData function

--- src/javascript/ch01/statement.js
+++ src/javascript/ch01/statement.js
@@ -16,12 +16,16 @@ function renderPlainText(data, plays) {
 }
 
 function statement(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
+    return renderPlainText(createStatementData(invoice, plays));
+
+    function createStatementData(invoice, plays) {
+        const statementData = {};
+        statementData.customer = invoice.customer;
+        statementData.performances = invoice.performances.map(enrichPerformance);
+        statementData.totalAmount = totalAmount(statementData);
+        statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+        return statementData;
+    }
 
     function totalVolumeCredits(data) {
         return data.performances
