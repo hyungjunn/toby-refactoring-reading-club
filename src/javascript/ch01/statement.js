ffca8f93b9e6dd17fe50222b6a262c2be3c497d6
hyungjunn <hotfirebulldog@gmail.com>
2025-01-17 10:40:21 +0900

    refactor: amountFor 호출하던 부분을 중간 데이터를 사용하도록 바꾸어라

--- src/javascript/ch01/statement.js
+++ src/javascript/ch01/statement.js
@@ -6,28 +6,6 @@ function renderPlainText(data, plays) {
             minimumFractionDigits: 2
         }).format;
 
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-        }
-        return result;
-    }
-
     function volumeCreditsFor(aPerformance) {
         let volumeCredits = 0;
         volumeCredits += Math.max(aPerformance.audience - 30, 0);
@@ -48,13 +26,13 @@ function renderPlainText(data, plays) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
 
     for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        result += ` ${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience}석)\n`;
     }
 
     result += `총액: ${usd(totalAmount() / 100)}\n`;
@@ -70,11 +48,34 @@ function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result)
+        result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
 
         function playFor(aPerformance) {
             return plays[aPerformance.playID];
+        }
+
+        function amountFor(aPerformance) {
+            let result = 0;
+            switch (aPerformance.play.type) {
+                case "tragedy":
+                    result = 40000;
+                    if (aPerformance.audience > 30) {
+                        result += 1000 * (aPerformance.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (aPerformance.audience > 20) {
+                        result += 10000 + 500 * (aPerformance.audience - 20);
+                    }
+                    result += 300 * aPerformance.audience;
+                    break;
+                default:
+                    throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+            }
+            return result;
         }
     }
 }
