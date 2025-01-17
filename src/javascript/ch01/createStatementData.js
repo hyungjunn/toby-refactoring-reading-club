bc394907cdba66767bf3621c0b30bc3db5923293
hyungjunn <hotfirebulldog@gmail.com>
2025-01-17 15:45:28 +0900

    refactor: inline instead of amountFor function

--- src/javascript/ch01/createStatementData.js
+++ src/javascript/ch01/createStatementData.js
@@ -49,8 +49,8 @@ export default function createStatementData(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
+        result.play = calculator.play
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result)
         return result;
     }
@@ -60,25 +60,7 @@ export default function createStatementData(invoice, plays) {
     }
 
     function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-        }
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
     }
 
     function volumeCreditsFor(aPerformance) {
