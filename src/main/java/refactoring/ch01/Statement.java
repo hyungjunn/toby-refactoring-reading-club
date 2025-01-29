8f26c359af90122a58f12e1bb39df9245d5faafd
hyungjunn <hotfirebulldog@gmail.com>
2025-01-29 21:42:19 +0900

    Inline amountFor(..)

--- src/main/java/refactoring/ch01/Statement.java
+++ src/main/java/refactoring/ch01/Statement.java
@@ -21,8 +21,7 @@ public class Statement {
 
         for (Performance perf : invoice.performances()) {
             Play play = plays.get(perf.playID());
-            double thisAmount = amountFor(perf, play);
-
+            
             // 포인트를 적립한다.
             volumeCredits += Math.max(perf.audience() - 30, 0);
             // 희극 관객 5명마다 추가 포인트를 제공한다.
@@ -31,8 +30,8 @@ public class Statement {
             }
 
             // 청구 내역을 출력한다.
-            result.append(String.format(" %s: %s (%d석)\n", play.name(), format.format(thisAmount / 100), perf.audience()));
-            totalAmount += thisAmount;
+            result.append(String.format(" %s: %s (%d석)\n", play.name(), format.format(amountFor(perf, play) / 100), perf.audience()));
+            totalAmount += amountFor(perf, play);
         }
         result.append(String.format("총액: %s\n", format.format(totalAmount / 100)));
         result.append(String.format("적립 포인트: %d점\n", volumeCredits));
