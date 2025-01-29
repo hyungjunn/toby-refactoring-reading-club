f5e6cdb6f4c7a5e3449498c22c9c68a07c0c0489
hyungjunn <hotfirebulldog@gmail.com>
2025-01-29 21:41:33 +0900

    Fix local variable to result

--- src/main/java/refactoring/ch01/Statement.java
+++ src/main/java/refactoring/ch01/Statement.java
@@ -40,24 +40,24 @@ public class Statement {
     }
 
     private static double amountFor(Performance perf, Play play) {
-        double thisAmount = 0;
+        double result = 0;
         switch (play.type()) {
             case "tragedy": // 비극
-                thisAmount = 40000;
+                result = 40000;
                 if (perf.audience() > 30) {
-                    thisAmount += 1000 * (perf.audience() - 30);
+                    result += 1000 * (perf.audience() - 30);
                 }
                 break;
             case "comedy": // 희극
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.audience() > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience() - 20);
+                    result += 10000 + 500 * (perf.audience() - 20);
                 }
-                thisAmount += 300 * perf.audience();
+                result += 300 * perf.audience();
                 break;
             default:
                 throw new IllegalArgumentException(String.format("알 수 없는 장르: %s", play.type()));
         }
-        return thisAmount;
+        return result;
     }
 }
