b17655e25e8dc53edd8dd31dd84bab57e61321d7
hyungjunn <hotfirebulldog@gmail.com>
2025-01-30 17:30:10 +0900

    Add renderHtml

--- src/main/java/refactoring/ch01/Statement.java
+++ src/main/java/refactoring/ch01/Statement.java
@@ -19,6 +19,25 @@ public class Statement {
         return renderPlainText(data);
     }
 
+    public String generateHtml() {
+        StatementData data = calculator.createStatementData(invoice);
+        return renderHtml(data);
+    }
+
+    private String renderHtml(StatementData data) {
+        StringBuilder result = new StringBuilder(String.format("청구 내역 (고객명: %s)\n", data.customer));
+        result.append("<table>\n");
+        result.append("<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n");
+        for (EnrichedPerformances perf : data.performances) {
+            result.append(String.format("<tr><td>%s</td><td>(%s석)</td>", perf.play().name(), perf.performance().audience()));
+            result.append(String.format("<td>%s</td></tr>\n", formatUSD(perf.amount())));
+        }
+        result.append("</table>\n");
+        result.append(String.format("<p>총액: <em>%s</em></p>\n", formatUSD(data.getTotalAmount())));
+        result.append(String.format("<p>적립 포인트: <em>%d</em>점</p>\n", data.getVolumeCredits()));
+        return result.toString();
+    }
+
     private String renderPlainText(StatementData data) {
         StringBuilder result = new StringBuilder(String.format("청구 내역 (고객명: %s)\n", data.customer));
         for (EnrichedPerformances perf : data.performances) {
@@ -28,16 +47,8 @@ public class Statement {
                     perf.performance().audience()));
         }
 
-        double totalAmount = data.performances.stream()
-                .mapToDouble(EnrichedPerformances::amount)
-                .sum();
-
-        int volumeCredits = data.performances.stream()
-                .mapToInt(EnrichedPerformances::volumeCredits)
-                .sum();
-
-        result.append(String.format("총액: %s\n", formatUSD(totalAmount)));
-        result.append(String.format("적립 포인트: %d점\n", volumeCredits));
+        result.append(String.format("총액: %s\n", formatUSD(data.getTotalAmount())));
+        result.append(String.format("적립 포인트: %d점\n", data.getVolumeCredits()));
         return result.toString();
     }
 
@@ -46,6 +57,17 @@ public class Statement {
     }
 
     record StatementData(String customer, List<EnrichedPerformances> performances) {
+        public double getTotalAmount() {
+            return performances.stream()
+                    .mapToDouble(EnrichedPerformances::amount)
+                    .sum();
+        }
+
+        public int getVolumeCredits() {
+            return performances.stream()
+                    .mapToInt(EnrichedPerformances::volumeCredits)
+                    .sum();
+        }
     }
 
     record EnrichedPerformances(
